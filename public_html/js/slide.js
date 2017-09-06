6442dc94c2c65c3ae4baac56437430f8d64a54c1
kele839228409 <839228409@qq.com>
2017-09-06 18:21:21 +0800

    添加test2

---
+++ public_html/js/slide.js
@@ -0,0 +1,5 @@
+$(function(){
+	jQuery(".slidebox").slide({mainCell:".bd ul",autoPlay:true});
+	jQuery(".picScroll").slide({ mainCell:"ul", effect:"leftLoop", vis:3, scroll:1,  autoPage:true});
+})
+
